1c8db1e13aebc1c2a5420b40161003824301fad4
rohithsenthilkumar <rohithsenthilkumar01@gmail.com>
2025-07-25 17:34:19 +0530

    added new code for designer door mat problem

--- designer_welcome_mat.py
+++ designer_welcome_mat.py
@@ -1,10 +1,10 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 def mat(n,m):
     for i in range(0,n//2):
-        print('-'*((n//2-i)*3) + '.|.'*(i*2+1) + '-'*((n//2-i)*3))
+        print( ('.|.'*(i*2+1)).center(m,"-"))
     print("WELCOME".center(m,'-'))
     for i in range(n//2-1,-1,-1):
-        print('-'*((n//2-i)*3) + '.|.'*(i*2+1) + '-'*((n//2-i)*3))
+        print(('.|.'*(i*2+1)).center(m,"-"))
     
 n, m = map(int , input().split())
 mat(int(n),int(m))
